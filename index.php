addacc9d3d1a72eed5b57bc9b5359d644dbec24d
mossaw <mt.sakem@gmail.com>
2019-12-24 22:48:35 +0900

    html追記

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>木舌プログラム</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
 <?php
 
 function kinoshita($i, $max = 100)
@@ -18,3 +30,6 @@ function kinoshita($i, $max = 100)
 }
 
 kinoshita(1);
+?>
+  </body>
+</html>
